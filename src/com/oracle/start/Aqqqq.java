c44d3ad82fb054af738a31e668ba78a61fae45e5
yangliu12138 <510529909@qq.com>
2019-12-06 09:14:36 +0800

    hjhjhjjh

--- src/com/oracle/start/Aqqqq.java
+++ src/com/oracle/start/Aqqqq.java
@@ -2,7 +2,7 @@ package com.oracle.start;
 
 public class Aqqqq {
 	public static void main(String[] args) {
-		System.out.println("3333333333333");
+		System.out.println("3333ggggggggggggggggg333333333");
 	}
 
 }
